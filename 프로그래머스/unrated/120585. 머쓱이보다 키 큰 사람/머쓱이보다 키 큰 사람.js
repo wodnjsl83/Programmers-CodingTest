d25e0c27e9434efc2254b57504b04aa36b6dcbd2
JaeWonE <119985100+wodnjsl83@users.noreply.github.com>
2023-10-23 16:02:29 +0900

    [level unrated] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,10 @@
+function solution(array,height){
+    let count = 0;
+    
+    for (let i=0; i<array.length; i++){
+        if(array[i]>height){
+            count ++;
+        }
+    }
+    return count;
+}
