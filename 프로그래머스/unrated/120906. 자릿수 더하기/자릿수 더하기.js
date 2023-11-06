50ad447bc713a1526618a3232700415483ea2345
JaeWonE <119985100+wodnjsl83@users.noreply.github.com>
2023-11-06 16:09:58 +0900

    [level unrated] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.js
@@ -2,7 +2,8 @@ function solution(n) {
     let a = 0;
     let b = n.toString();
     for(let i = 0; i<b.length; i++){
-        a += parseInt(b[i])
+        //parseInt도 가능
+        a += Number(b[i])
     }
     return a;
 }
